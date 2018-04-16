c63d546222812ff2c1d445a1bcb15be1d28d3890
hezi <495556609@qq.com>
2018-04-17 01:39:23 +0800

    second

--- main_1.js
+++ main_1.js
@@ -49,7 +49,7 @@ if(current === $buttons.length -1 && index === 0){
     .one('transitionend', function(){
         $slides.hide()
         $slides.offset() // .offset() 可以触发 re-layout，这是一个高级技术，删掉这行你就会发现 bug，所以只能加上这一行。
-        // 不要写邮件来问我为什么要写 .offset，你自己注释掉上面一行看最后一张到第一张的动画，就知道为什么要加 offset() 了。
+        
         $slides.css({transform:`translateX(${-(index+1)*400}px)`}).show()
     })
 
